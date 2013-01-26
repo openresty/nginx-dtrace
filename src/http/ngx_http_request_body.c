a3e470216713fca314e186cf2cd0d6e19c588728
agentzh (Yichun Zhang) <agentzh@gmail.com>
2013-01-26 15:25:16 -0800

    reversed applied the allow_request_body_updating patch.

--- src/http/ngx_http_request_body.c
+++ src/http/ngx_http_request_body.c
@@ -39,7 +39,7 @@ ngx_http_read_client_request_body(ngx_http_request_t *r,
 
     r->main->count++;
 
-    if (r->request_body || r->discard_body || r->content_length_n == 0) {
+    if (r->request_body || r->discard_body) {
         post_handler(r);
         return NGX_OK;
     }
@@ -448,7 +448,7 @@ ngx_http_discard_request_body(ngx_http_request_t *r)
     ssize_t       size;
     ngx_event_t  *rev;
 
-    if (r != r->main || r->discard_body || r->content_length_n == 0) {
+    if (r != r->main || r->discard_body) {
         return NGX_OK;
     }
 
@@ -464,22 +464,20 @@ ngx_http_discard_request_body(ngx_http_request_t *r)
         ngx_del_timer(rev);
     }
 
-    r->content_length_n = r->headers_in.content_length_n;
-
-    if (r->content_length_n <= 0 || r->request_body) {
+    if (r->headers_in.content_length_n <= 0 || r->request_body) {
         return NGX_OK;
     }
 
     size = r->header_in->last - r->header_in->pos;
 
     if (size) {
-        if (r->content_length_n > size) {
+        if (r->headers_in.content_length_n > size) {
             r->header_in->pos += size;
-            r->content_length_n -= size;
+            r->headers_in.content_length_n -= size;
 
         } else {
-            r->header_in->pos += (size_t) r->content_length_n;
-            r->content_length_n = 0;
+            r->header_in->pos += (size_t) r->headers_in.content_length_n;
+            r->headers_in.content_length_n = 0;
             return NGX_OK;
         }
     }
@@ -580,7 +578,7 @@ ngx_http_read_discarded_request_body(ngx_http_request_t *r)
                    "http read discarded body");
 
     for ( ;; ) {
-        if (r->content_length_n == 0) {
+        if (r->headers_in.content_length_n == 0) {
             r->read_event_handler = ngx_http_block_reading;
             return NGX_OK;
         }
@@ -589,9 +587,9 @@ ngx_http_read_discarded_request_body(ngx_http_request_t *r)
             return NGX_AGAIN;
         }
 
-        size = (r->content_length_n > NGX_HTTP_DISCARD_BUFFER_SIZE) ?
+        size = (r->headers_in.content_length_n > NGX_HTTP_DISCARD_BUFFER_SIZE) ?
                    NGX_HTTP_DISCARD_BUFFER_SIZE:
-                   (size_t) r->content_length_n;
+                   (size_t) r->headers_in.content_length_n;
 
         n = r->connection->recv(r->connection, buffer, size);
 
@@ -608,7 +606,7 @@ ngx_http_read_discarded_request_body(ngx_http_request_t *r)
             return NGX_OK;
         }
 
-        r->content_length_n -= n;
+        r->headers_in.content_length_n -= n;
     }
 }
 
